d84e53c98681a4df55bb6be5c0f9074936a1898e
Derek Blankenship <derekblankenship05@gmail.com>
2018-01-31 20:18:22 -0500

    fixed(?) a bug with external javascript
    ESLint gives errors when you look at the js code, but the js code runs
    fine when you open up the webpage in live preview.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function dropTopnav() {
+    var x = document.getElementById("js-topnav");
+    if (x.className === "topnav") {
+        x.className += " topnav--dropped";
+    } 
+    else {
+        x.className = "topnav";
+    }
+}
