cd84433604b0f7c1e781f19b4353f12782246ac7
Derek Blankenship <derekblankenship05@gmail.com>
2018-03-09 16:30:49 -0500

    Added some project previews on the home page
    with fancy fade-out/fade-in boxes!

--- topnav.js
+++ topnav.js
@@ -2,6 +2,7 @@
 
 var topnav = document.getElementById("js-topnav");
 var topnavBackground = document.getElementById("js-topnav__background");
+var content = document.getElementById("js-content");
 var sticky = topnav.offsetTop + 38;
 
 window.onscroll = function() {makeStickyTopnav()};
@@ -9,9 +10,11 @@ window.onscroll = function() {makeStickyTopnav()};
 function dropTopnav() {
     if (!topnav.classList.contains("topnav--dropped")) {
         topnav.classList.add("topnav--dropped");
+        topnavBackground.classList.add("topnav__background--dropped");
     } 
     else {
         topnav.classList.remove("topnav--dropped");
+        topnavBackground.classList.remove("topnav__background--dropped");
     }
 }
 
@@ -21,10 +24,12 @@ function makeStickyTopnav() {
     if (window.pageYOffset >= sticky) {
         topnav.classList.add("topnav--sticky")
         topnavBackground.classList.add("topnav__background--sticky");
+        content.classList.add("content--scrolled");
     } 
     else {
         topnav.classList.remove("topnav--sticky");
         topnavBackground.classList.remove("topnav__background--sticky");
+        content.classList.remove("content--scrolled");
     }
 } 
 
