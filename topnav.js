6504f2f864d2bebfa746413dc978402747e1fb51
nyghtly-derek <derekblankenship05@gmail.com>
2018-07-12 12:39:58 -0400

    Added splash page, updated nav bar Also refactored a bunch of code and added css variables

--- topnav.js
+++ topnav.js
@@ -1,35 +1,19 @@
 
 
 var topnav = document.getElementById("js-topnav");
-var topnavBackground = document.getElementById("js-topnav__background");
-var content = document.getElementById("js-content");
-var sticky = topnav.offsetTop + 38;
 
-window.onscroll = function() {makeStickyTopnav()};
 
 function dropTopnav() {
     if (!topnav.classList.contains("topnav--dropped")) {
         topnav.classList.add("topnav--dropped");
-        topnavBackground.classList.add("topnav__background--dropped");
     } 
     else {
         topnav.classList.remove("topnav--dropped");
-        topnavBackground.classList.remove("topnav__background--dropped");
     }
 }
 
+function pullOutNavElem() {
+    // TODO: animate elems on hover
+}
 
-
-function makeStickyTopnav() {
-    if (window.pageYOffset >= sticky) {
-        topnav.classList.add("topnav--sticky")
-        topnavBackground.classList.add("topnav__background--sticky");
-        content.classList.add("content--scrolled");
-    } 
-    else {
-        topnav.classList.remove("topnav--sticky");
-        topnavBackground.classList.remove("topnav__background--sticky");
-        content.classList.remove("content--scrolled");
-    }
-} 
 
